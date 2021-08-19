e3b36a64c08fa5636f10a6276a8f359809d5e22f
Liel <liel.izcha321@gmail.com>
2021-08-19 18:27:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,94 @@
+// Exercise 1
+// function Hello() {
+//   document.write("hello there");
+// }
+// Hello()
+
+// Exercise 2
+
+// function helloDavid() {
+//   document.write("hello there david");
+// }
+// helloDavid()
+
+// Exercise 3
+// var userName = prompt("ur name")
+// function helloInput(inpUser){
+//    document.write( `hello there ${inpUser}`)
+
+// }
+// helloInput(userName)
+
+// Exercise 4
+// var userName = prompt("name"),
+// userLname = prompt("last name"),
+// UserAge = prompt("age")
+// function PrintInWindow(inp1, inp2, inp3) {
+//   document.write(`${inp1} , ${inp2} , ${inp3}`);
+// }
+
+// PrintInWindow(userName, userLname, UserAge);
+
+// Exercise 5
+// var userName = prompt("first name");
+// var lName = prompt("last name");
+// var userAge = prompt("age");
+// function fullName(userInput1, userInput2, userInput3) {
+//   document.getElementById(
+//     "myDiv"
+//   ).innerText = `${userInput1} , ${userInput2} , ${userInput3}`;
+// }
+// fullName(userName, lName, userAge);
+
+// Exercise 6
+// var nameUser = prompt("first name");
+// var LNameUser = prompt("last name");
+// var AgeUser = prompt("age");
+// function divInDiv(userInput1, userInput2, userInput3) {
+//   document.getElementById(
+//     "exe9"
+//   ).innerHTML = `<div>${userInput1}</div> <div>${userInput2}</div>  <div>${userInput3}</div>`;
+// }
+// divInDiv(nameUser, LNameUser, AgeUser);
+
+// Exercise 7
+// let userAge = prompt("age"),
+//   userName = prompt("name");
+// function toWindow(inp1 , inp2) {
+//     document.write(`<div>myName:${inp1}, age:${inp2}</div> `)
+// }
+// toWindow(userName,userAge  )
+
+// Exercise 8
+
+// var Finame = prompt("first name");
+// FLname = prompt("last name");
+// age = +prompt("age");
+// function writeToWindo(input1, input2, input3) {
+//   document.write(
+//     `<div><p>first name: ${input1} , last name: ${input2} , age : ${input3}`
+//   );
+// }
+// writeToWindo(Finame, FLname, age);
+
+// Exercise 9
+// var fName = prompt("first name")
+//   lName = prompt("last name");
+//   ageUseR = prompt("age");
+
+// function writeWithId(inp1, inp2, inp3) {
+//   document.write(`<P id= "try">name :${inp1} , fName:${inp2} , age:${inp3}`);
+// }
+// writeWithId(fName, lName, ageUseR);
+// Exercise 10
+
+// what is this?
+// var divs = document.getElementsByTagName("div");
+// function changWith() {
+//   for (let i = 0; i < divs.length; i++) {
+//     divs[i].style = `width: ${Math.floor(
+//       Math.random() * 1000
+//     )}px; height:${Math.floor(Math.random() * 100)}px;}`;
+//   }
+// }
+// changWith();
